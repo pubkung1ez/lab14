204e5790e3ec4191dccb9be77fa1e54f456e1600
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-01-09 17:00:15 +0700

    suss

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,61 @@ int main(){
 }
 
 //Do not modify source code above this line 
+void showMatrix(const bool an[][N]){
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < N ; j++){
+            cout << an [i][j]<< " ";
+        }
+        cout << "\n";
+    }
+}
+
+void inputMatrix(double bn[][N]){
+    for(int i = 0 ; i < N ; i++){
+        cout << "Row "<<i+1<<  ": ";
+        for(int j = 0 ; j < N ; j++){
+            cin >> bn [i][j];
+        }
+    }
+}
+
+bool taokub(double x,double y){
+    if(x >= y){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool taokub1(bool a,bool b,bool c,bool d){
+    if(a and b and c and d ){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int findloca(const double aa[][N],int i,int j){
+    if(i == 0 || j == 0 || i == N-1 || j == N-1){
+        return 0;
+    }else{
+        double center = aa[i][j];
+        bool a = taokub(center , aa[i-1][j]);
+        bool b = taokub(center , aa[i][j+1]);
+        bool c = taokub(center , aa[i][j-1]);
+        bool d = taokub(center , aa[i+1][j]);
+        if(taokub1(a,b,c,d)){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
+
+void findLocalMax(const double aa[][N], bool bb[][N]){
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < N ; j++){
+            bb[i][j] = findloca(aa,i,j);
+        }
+    }
+}
